d225a902ad4af760e5e93d6f2d4e77d0ccf574b3
nourhene chalghoumi <115397025+nourhenechalghoumi@users.noreply.github.com>
2023-11-06 18:13:27 -0800

    change junit

---
+++ src/test/services/InvoiceServiceImplTest.java
@@ -0,0 +1,89 @@
+package tn.esprit.devops_project.services;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.repository.query.Param;
+import tn.esprit.devops_project.entities.Invoice;
+import tn.esprit.devops_project.entities.Operator;
+import tn.esprit.devops_project.entities.Supplier;
+import tn.esprit.devops_project.repositories.InvoiceDetailRepository;
+import tn.esprit.devops_project.repositories.InvoiceRepository;
+import tn.esprit.devops_project.repositories.OperatorRepository;
+import tn.esprit.devops_project.repositories.SupplierRepository;
+import tn.esprit.devops_project.services.InvoiceServiceImpl;
+
+public class InvoiceServiceImplTest {
+
+    @InjectMocks
+    private InvoiceServiceImpl invoiceService;
+
+    @Mock
+    private InvoiceRepository invoiceRepository;
+    @Mock
+    private InvoiceDetailRepository invoiceDetailRepository;
+    @Mock
+    private OperatorRepository operatorRepository;
+
+    @Mock
+    private SupplierRepository supplierRepository;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testRetrieveAllInvoices() {
+        List<Invoice> invoices = new ArrayList<>();
+        invoices.add(new Invoice());
+        when(invoiceRepository.findAll()).thenReturn(invoices);
+        List<Invoice> retrievedInvoices = invoiceService.retrieveAllInvoices();
+        assertEquals(1, retrievedInvoices.size());
+    }
+
+    @Test
+    void testCancelInvoice() {
+        Long invoiceId = 1L;
+        Invoice invoice = new Invoice();
+        invoice.setArchived(false);
+        when(invoiceRepository.findById(invoiceId)).thenReturn(Optional.of(invoice));
+        invoiceService.cancelInvoice(invoiceId);
+        verify(invoiceRepository, times(1)).save(invoice);
+        assertTrue(invoice.getArchived());
+    }
+
+    @Test
+    void testRetrieveInvoice() {
+        Long invoiceId = 1L;
+        Invoice invoice = new Invoice();
+        when(invoiceRepository.findById(invoiceId)).thenReturn(Optional.of(invoice));
+        Invoice retrievedInvoice = invoiceService.retrieveInvoice(invoiceId);
+        assertEquals(invoice, retrievedInvoice);
+    }
+
+
+
+////////////////////////////////////////////////////////////////
+    @Test
+    void testGetTotalAmountInvoiceBetweenDates() {
+        Date startDate = new Date();
+        Date endDate = new Date();
+        float totalAmount = 100.0f;
+        when(invoiceRepository.getTotalAmountInvoiceBetweenDates(startDate, endDate)).thenReturn(totalAmount);
+        float result = invoiceService.getTotalAmountInvoiceBetweenDates(startDate, endDate);
+        assertEquals(totalAmount, result);
+    }
+
+
+}
---
+++ src/test/services/StockServiceImplTest.java
@@ -0,0 +1,83 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.devops_project.entities.Stock;
+import tn.esprit.devops_project.repositories.StockRepository;
+import tn.esprit.devops_project.services.StockServiceImpl;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StockServiceImplTest {
+
+    @InjectMocks
+    private StockServiceImpl stockService;
+
+    @Mock
+    private StockRepository stockRepository;
+
+    @Test
+    void addStock() {
+        Stock stockToAdd = new Stock();
+        stockToAdd.setTitle("Title");
+
+        Stock savedStock = new Stock();
+        savedStock.setIdStock(1L);
+        savedStock.setTitle("Title");
+
+        // Mock the behavior of the repository when saving a stock
+        when(stockRepository.save(stockToAdd)).thenReturn(savedStock);
+
+        // Call the service method
+        Stock addedStock = stockService.addStock(stockToAdd);
+
+        // Verify the service behavior
+        assertEquals(addedStock.getIdStock(), 1L);
+        assertEquals(addedStock.getTitle(), "Title");
+    }
+
+    @Test
+    void retrieveStock() {
+        Stock stockToRetrieve = new Stock();
+        stockToRetrieve.setIdStock(1L); // Set the ID
+        stockToRetrieve.setTitle("stock 1");
+
+
+        // Mock the behavior of the repository when retrieving a stock by ID
+        when(stockRepository.findById(1L)).thenReturn(Optional.of(stockToRetrieve));
+
+        // Call the service method
+        Stock retrievedStock = stockService.retrieveStock(1L);
+
+        // Verify the service behavior
+        assertEquals(retrievedStock.getIdStock(), 1L);
+        assertEquals(retrievedStock.getTitle(), "stock 1");
+    }
+
+    @Test
+    void retrieveAllStock() {
+        Stock stock = new Stock();
+        stock.setIdStock(1L);
+        stock.setTitle("stock 1");
+
+        // Mock the behavior of the repository when retrieving all stocks
+        when(stockRepository.findAll()).thenReturn(Collections.singletonList(stock));
+
+        // Call the service method
+        List<Stock> allStocks = stockService.retrieveAllStock();
+
+        // Verify the service behavior
+        assertEquals(allStocks.size(), 1);
+        assertEquals(allStocks.get(0).getIdStock(), 1L);
+        assertEquals(allStocks.get(0).getTitle(), "stock 1");
+    }
+}
